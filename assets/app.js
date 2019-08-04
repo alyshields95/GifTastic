8dbbc65fab5e141e0c5f8c460f550648e932dce6
Alexandra Shields <alexandrashields@Alexandras-MacBook-Pro.local>
2019-08-04 07:08:12 -0400

    styling

--- assets/app.js
+++ assets/app.js
@@ -1,24 +1,28 @@
 $(document).ready(function () {
 
-            var topics = [
-                "Mickey Mouse",
-                "Minnie Mouse",
-                "Cinderlla",
-                "Sleeping Beauty",
-                "Elsa"
-            ]
+    var topics = [
+        "Mickey Mouse",
+        "Minnie Mouse",
+        "Cinderlla",
+        "Rapunzel",
+        "Elsa"
+    ]
 
-            var API_KEY = "awz6IBjPW39QSis3556Ny8cvAAszbvv7";
-            var requestUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + API_KEY + "&limit=10&q=";
+    var API_KEY = "awz6IBjPW39QSis3556Ny8cvAAszbvv7";
+    var requestUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + API_KEY + "&limit=10&q=";
 
 
-            for (var i = 0; i < topics.length; i++) {
-                var button = $("<button>");
-                $("#buttons").append(button);
-                button.addClass("btn btn-info topic");
-                button.text(topics[i]);
-            };
+    for (var i = 0; i < topics.length; i++) {
+        var button = $("<button>");
+        $("#buttons").append(button);
+        button.addClass("btn btn-info topic");
+        button.text(topics[i]);
+    };
 
-            function createImage(response, i) {
-                var img = $("<img class='giphy-img'>");
-                var rating = $("<h2>Rating: </h2>" + "<p>" + response.data[i].rating + "</p>");
+
+
+
+
+
+
+});
