60c95fad52f4396281b017b2c413044d141df1d5
Alexandra Shields <alexandrashields@Alexandras-MacBook-Pro.local>
2019-08-04 11:34:43 -0400

    create image funtion and image attributes

--- assets/app.js
+++ assets/app.js
@@ -1,28 +1,35 @@
 $(document).ready(function () {
 
-    var topics = [
-        "Mickey Mouse",
-        "Minnie Mouse",
-        "Cinderlla",
-        "Rapunzel",
-        "Elsa"
-    ]
+            var topics = [
+                "Mickey Mouse",
+                "Minnie Mouse",
+                "Cinderlla",
+                "Rapunzel",
+                "Elsa"
+            ]
 
-    var API_KEY = "awz6IBjPW39QSis3556Ny8cvAAszbvv7";
-    var requestUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + API_KEY + "&limit=10&q=";
-
-
-    for (var i = 0; i < topics.length; i++) {
-        var button = $("<button>");
-        $("#buttons").append(button);
-        button.addClass("btn btn-info topic");
-        button.text(topics[i]);
-    };
+            var API_KEY = "awz6IBjPW39QSis3556Ny8cvAAszbvv7";
+            var requestUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + API_KEY + "&limit=10&q=";
 
 
+            for (var i = 0; i < topics.length; i++) {
+                var button = $("<button>");
+                $("#buttons").append(button);
+                button.addClass("btn btn-info topic");
+                button.text(topics[i]);
+            };
+
+
+            function createImage(response, i) {
+                var img = $("<img class='giphy-img'>");
+                var rating = $("<h2>Rating: </h2>" + "<p>" + response.data[i].rating + "</p>");
+
+                img.attr("src", response.data[i].images.downsized_still.url);
+                img.attr("data-animated", response.data[i].images.downsized.url);
+                img.attr("data-still", response.data[i].images.downsized_still.url);
+                img.attr("data-state", "still");
 
 
 
 
-
-});
+            });
